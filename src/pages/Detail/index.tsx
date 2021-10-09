7703c719977ee83ee076e66c3b2e353062cc9248
FrankD6EAF <frankd6eaf@gmail.com>
2021-10-09 19:48:37 +0800

    update

--- src/pages/Detail/index.tsx
+++ src/pages/Detail/index.tsx
@@ -10,10 +10,10 @@ import aave_logo from "../../assets/images/J1YJtvdI_400x400.jpeg";
 import heat from "../../assets/images/icons/heat.svg";
 import heat_active from "../../assets/images/icons/heat_active.svg";
 import eth from "../../assets/images/eth.svg";
-import {useTranslation} from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 const Detail: React.FC = ({}) => {
-  const {t,i18n} = useTranslation()
+  const { t, i18n } = useTranslation();
   const col: any = [
     {
       title: "#",
@@ -26,7 +26,7 @@ const Detail: React.FC = ({}) => {
     },
     {
       title() {
-        return t('USER')
+        return t("USER");
       },
       dataIndex: "address",
       key: "address",
@@ -37,7 +37,7 @@ const Detail: React.FC = ({}) => {
     },
     {
       title() {
-        return t('NET WORTH')
+        return t("NET WORTH");
       },
       dataIndex: "value",
       key: "value",
@@ -48,7 +48,7 @@ const Detail: React.FC = ({}) => {
     },
     {
       title() {
-        return t('SHARE')
+        return t("SHARE");
       },
       dataIndex: "cur_rate",
       key: "cur_rate",
@@ -59,7 +59,7 @@ const Detail: React.FC = ({}) => {
     },
     {
       title() {
-        return t('CUMULATIVE SHARE')
+        return t("CUMULATIVE SHARE");
       },
       dataIndex: "top_rate",
       key: "top_rate",
@@ -70,7 +70,7 @@ const Detail: React.FC = ({}) => {
     },
     {
       title() {
-        return t('UPDATED AT')
+        return t("UPDATED AT");
       },
       dataIndex: "date",
       key: "date",
@@ -124,14 +124,14 @@ const Detail: React.FC = ({}) => {
     setToken(name);
   }, [name]);
   return (
-    <div style={{paddingBottom: 80}}>
+    <div style={{ paddingBottom: 80 }}>
       <Header />
       <div className="detail-wrap">
         <div className="detail-box">
           <div className="p50">
             <Link to="/" className="flex items-center">
-              <i className="fas fa-angle-left" style={{fontSize: 16}}/>
-              <span style={{ marginLeft: 15 }}>{t('Protocol Detail')}</span>
+              <i className="fas fa-angle-left" style={{ fontSize: 16 }} />
+              <span style={{ marginLeft: 15 }}>{t("Protocol Detail")}</span>
             </Link>
             <div className="top-title flex items-center mt-12">
               <img className="toptitle-logo mr-4" src={logo} alt="" />
@@ -148,33 +148,39 @@ const Detail: React.FC = ({}) => {
                 {url}
               </a>
               <span className="spliteline flex items-center">
-                <span className="label-1 text-gray-1">{t('On the chain')}</span>
-                <img className="label-1" src={eth} alt=""/>
+                <span className="label-1 text-gray-1">{t("On the chain")}</span>
+                <img className="label-1" src={eth} alt="" />
                 <span>Ethereum</span>
               </span>
               <span className="spliteline flex items-center">
-                <span className="label-1 text-gray-1">{t('Platform token')}</span>
+                <span className="label-1 text-gray-1">
+                  {t("Platform token")}
+                </span>
                 <img className="logo label-1" src={logo} alt="" />
                 <span>{token}</span>
               </span>
             </div>
             <div className="detail-sub flex">
               <span>
-                <span className="label-1 e text-gray-2">{t('Genesis date')}</span>
+                <span className="label-1 e text-gray-2">
+                  {t("Genesis date")}
+                </span>
                 <span className="label-detail">2020/01/02</span>
               </span>
               <span className="ml-4">
-                <span className="label-1 e text-gray-2">{t('Total users')}</span>
+                <span className="label-1 e text-gray-2">
+                  {t("Total users")}
+                </span>
                 <span className="label-detail">{"1" + random(10000)}</span>
               </span>
               <span className="ml-4">
-                <span className="label-1 e text-gray-2">{t('24h Users')}</span>
+                <span className="label-1 e text-gray-2">{t("24h Users")}</span>
                 <span className="label-detail">{random(3000)}</span>
               </span>
             </div>
 
             <div className="mt-20">
-              <span className="s1 mr-8 text-gray-2">{t('User deposits')}</span>
+              <span className="s1 mr-8 text-gray-2">{t("User deposits")}</span>
               <span className="s2">{`$${random(100)},` + random(1000)}</span>
             </div>
           </div>
@@ -192,42 +198,52 @@ const Detail: React.FC = ({}) => {
             <div className="flex mb-10">
               <div className="mr-10 flex items-center ">
                 <i className="label-1-icon fab fa-twitter" />
-                <span className="mr-4 label-1 e flex">{t('Twitter Heat')}:</span>
-                <span className="flex gap-2 badge">
-                <img src={heat_active} />
-                <img src={heat_active} />
-                <img src={heat_active} />
-                <img src={heat_active} />
-                <img src={heat} alt=""/>
-              </span>
+                <span className="mr-2 label-1 e flex">
+                  {t("Twitter Heat")}:
+                </span>
+                <span className="flex gap-0.5 badge">
+                  <img src={heat_active} />
+                  <img src={heat_active} />
+                  <img src={heat_active} />
+                  <img src={heat_active} />
+                  <img src={heat} alt="" />
+                </span>
               </div>
               <div className=" flex items-center badge">
                 <i className="label-1-icon fab fa-telegram-plane" />
-                <span className="mr-4 label-1 e flex">{t('Telegram Heat')}:</span>
-                <span className="flex gap-2">
-                <img src={heat_active} />
-                <img src={heat_active} />
-                <img src={heat_active} />
-                <img src={heat} />
-                <img src={heat} alt=""/>
-              </span>
+                <span className="mr-2 label-1 e flex">
+                  {t("Telegram Heat")}:
+                </span>
+                <span className="flex gap-0.5">
+                  <img src={heat_active} />
+                  <img src={heat_active} />
+                  <img src={heat_active} />
+                  <img src={heat} />
+                  <img src={heat} alt="" />
+                </span>
               </div>
             </div>
 
-            <div className="data items-center">
-              <div className="mr-4 label-1 e">{t('Twitter Sentiment Data')}</div>
+            <div className="data items-center leading-none">
+              <div className="mr-4 label-1 e" style={{ marginBottom: 5 }}>
+                {t("Twitter Sentiment Data")}
+              </div>
               <a className="" href="/">
                 https://ipfs.io/ipfs/MrnPLzHJ7rjddh2MZxAQmZjA7Go4WyJGqVXcbuA5GRtrx8
               </a>
             </div>
-            <div className="data items-center">
-              <div className="mr-4 label-1 e">{t('Holders Address')}</div>
+            <div className="data items-center leading-none">
+              <div className="mr-4 label-1 e" style={{ marginBottom: 5 }}>
+                {t("Holders Address")}
+              </div>
               <a href="/">
                 https://ipfs.io/ipfs/GRtrx8Qm7rjddh2MZxAXcbZjA7Go4WyJGqVzHJuAMrnPL5
               </a>
             </div>
-            <div className="data items-center">
-              <div className="mr-4 label-1 e">{t('Profitability On The Chain')}</div>
+            <div className="data items-center leading-none">
+              <div className="mr-4 label-1 e" style={{ marginBottom: 5 }}>
+                {t("Profitability On The Chain")}
+              </div>
               <a href="/">
                 https://ipfs.io/ipfs/VzHJ7rjddbuAMrnPL5GRtrx8QmZjA7Go4WyJGqh2MZxAXc
               </a>
